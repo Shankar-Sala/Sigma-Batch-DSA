c639c6de2cafea849ce47d23c7d7caa9fa32b0bd
Shankar-Sala <shankarsala9995@gmail.com>
2024-12-19 03:41:49 +0530

    chp-11

---
+++ 11.Operators/operators.java
@@ -0,0 +1,52 @@
+//Arithmetic Operators
+// Binary operators
+// public class operators{
+//     public static void main(String args[]){
+//         int A = 10;
+//         int B = 5;
+//         System.out.println("Addition = " + (A+B));
+//         System.out.println("Subtract = " + (A-B));
+//         System.out.println("Multiply = " + (A*B));
+//         System.out.println("Divide = " + (A/B));
+//         System.out.println("Modulo(remainder) = " + (A%B));
+//     }
+// }
+
+//Unary Operators
+// public class operators{
+//     public static void main(String args[]){
+//         int a = 10;
+//         int b = a--;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// }
+
+
+//Relational Operators
+// public class operators{
+//     public static void main(String args[]){
+//         int a = 210;
+//         int b = 10;
+//         System.out.println(a != b);
+//     }
+// }
+
+
+//Logical Operators
+// public class operators{
+//     public static void main(String args []){
+//         System.out.println((3>2) && (5<10));
+//     }
+// }
+
+
+// Assignment Operators
+public class operators{
+    public static void main(String args[]){
+        int A = 10;
+        // A = A + 10;
+        A +=10;
+        System.out.println(A);
+    }
+}
