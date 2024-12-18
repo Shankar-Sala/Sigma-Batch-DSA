2b1d4bd5f44c6d264b747ab9e6316b89a4be7110
Shankar-Sala <shankarsala9995@gmail.com>
2024-12-19 02:09:15 +0530

    chp-10

---
+++ 10.Variables_&_Datatypes/typeCasting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class typeCasting{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // float a = 25.12f;
+        // int b = (int)a;
+        // System.out.println(b);
+
+        char ch = 'a';
+        int num = ch;
+        System.out.println(num);
+    }
+}
